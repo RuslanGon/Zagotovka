9191256b402522c3b5f7df7a0e85adcc14e79d04
RuslanGon <ruslan441006@gmail.com>
2025-09-04 16:30:18 +0300

    wer

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import startServer from './db.js';
+
+
+
+dotenv.config(); 
+
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json())
+
+
+
+
+
+
+
+startServer(app);
